2b037683540f0eba4838c2259f4d3e90722d1a81
NicoAndres1 <nikoan97@hotmail.com>
2019-01-23 10:53:01 -0300

    BuildControls v1.3
    Some CSS changes

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -6,23 +6,25 @@ import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.module.css';
 
 const controls = [
-    { label: 'salad', type: 'salad' },
-    { label: 'cheese', type: 'cheese' },
-    { label: 'bacon', type: 'bacon' },
-    { label: 'meat', type: 'meat' }
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(ctlr => (
-            <BuildControl
-                key = {ctlr.label}
-                label = {ctlr.label}
-                added = {() => props.ingredientAdded(ctlr.type)}
-                removed = {() => props.ingredientRemoved(ctlr.type)}
-                disabled = {props.disabledIngredient[ctlr.type]}
-            />
-        ))}
+        <div className={classes.BuildControlsBox}>
+            {controls.map(ctlr => (
+                <BuildControl
+                    key={ctlr.label}
+                    label={ctlr.label}
+                    added={() => props.ingredientAdded(ctlr.type)}
+                    removed={() => props.ingredientRemoved(ctlr.type)}
+                    disabled={props.disabledIngredient[ctlr.type]}
+                />
+            ))}
+        </div>
     </div>
 );
 
