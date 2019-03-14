5a904b70ee26948f4a1fcf4b0b71d7d3cc5781de
NicoAndres1 <nikoan97@hotmail.com>
2019-03-13 21:53:06 -0300

    CheckOut IS WORKING!!
    Finally, CheckOut and ContactData are working, this commit is super important

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -96,36 +96,16 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice.toFixed(2),
-        //     customer: {
-        //         name: 'Nicolas Andres',
-        //         address: {
-        //             street: 'Calasanz',
-        //             number: '450',
-        //             zipCode: '7600',
-        //             country: 'Argentina'
-        //         },
-        //         email: 'nikoan97@hotmail.com'
-        //     },
-        //     deliveryHour: 'Right now'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         })
-        //     })
-        //     .catch(error => {
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         })
-        //     });
-        this.props.history.push('/checkout', {...this.state.ingredients});
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     };
 
     render() {
